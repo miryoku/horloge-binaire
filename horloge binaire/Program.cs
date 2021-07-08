b588e7eca614e77151df2853045502d3e718b3e5
a.stevens <ata_@hotmail.fr>
2021-07-08 11:14:52 +0200

    ajout du mode digital et de lire un fichier pour faire sonne une alarme

--- horloge binaire/Program.cs
+++ horloge binaire/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Threading;
 
 namespace horloge_binaire
@@ -7,6 +9,222 @@ namespace horloge_binaire
     {
         static void Main(string[] args)
         {
+
+            List<String> pause = ReadFile();
+
+            Heure(pause);
+
+
+            /* CancellationTokenSource cts = new CancellationTokenSource();
+
+              ThreadPool.QueueUserWorkItem(new WaitCallback(TetrisMusic), cts.Token);
+
+
+              // // Request cancellation.
+              cts.Cancel();
+              // Console.WriteLine("Fin du thread");             
+              cts.Dispose();
+
+             */
+
+            int heure = Int32.Parse(DateTime.Now.ToString("HH"));
+            int minute = Int32.Parse(DateTime.Now.ToString("mm"));
+            int seconde = Int32.Parse(DateTime.Now.ToString("ss"));
+            // Digital(heure, minute, seconde);
+
+        }
+
+        static List<String> ReadFile()
+        {
+            List<String> pause = new List<string>();
+            try
+            {
+                //Créez une instance de StreamReader pour lire à partir d'un fichier
+                using (StreamReader sr = new StreamReader($"C:\\Users\\{System.Environment.UserName}\\Desktop\\horloge.txt"))
+                {
+                    string line;
+                    // Lire les lignes du fichier jusqu'à la fin.
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        // Console.WriteLine(line);
+                        pause.Add(line);
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Le fichier n'a pas pu être lu.");
+                Console.WriteLine(e.Message);
+            }
+            return pause;
+        }
+
+
+
+
+
+        static void MarioMusic(Object ct)
+        {
+            CancellationToken token = (CancellationToken)ct;
+            Console.Beep(659, 125); Console.Beep(659, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(167); Console.Beep(523, 125); Console.Beep(659, 125); Thread.Sleep(125); Console.Beep(784, 125); Thread.Sleep(375); Console.Beep(392, 125); Thread.Sleep(375); Console.Beep(523, 125); Thread.Sleep(250); Console.Beep(392, 125); Thread.Sleep(250); Console.Beep(330, 125); Thread.Sleep(250); Console.Beep(440, 125); Thread.Sleep(125); Console.Beep(494, 125); Thread.Sleep(125); Console.Beep(466, 125); Thread.Sleep(42); Console.Beep(440, 125); Thread.Sleep(125); Console.Beep(392, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(125); Console.Beep(784, 125); Thread.Sleep(125); Console.Beep(880, 125); Thread.Sleep(125); Console.Beep(698, 125); Console.Beep(784, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(125); Console.Beep(523, 125); Thread.Sleep(125); Console.Beep(587, 125); Console.Beep(494, 125); Thread.Sleep(125); Console.Beep(523, 125); Thread.Sleep(250); Console.Beep(392, 125); Thread.Sleep(250); Console.Beep(330, 125); Thread.Sleep(250); Console.Beep(440, 125); Thread.Sleep(125); Console.Beep(494, 125); Thread.Sleep(125); Console.Beep(466, 125); Thread.Sleep(42); Console.Beep(440, 125); Thread.Sleep(125); Console.Beep(392, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(125); Console.Beep(784, 125); Thread.Sleep(125); Console.Beep(880, 125); Thread.Sleep(125); Console.Beep(698, 125); Console.Beep(784, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(125); Console.Beep(523, 125); Thread.Sleep(125); Console.Beep(587, 125); Console.Beep(494, 125); Thread.Sleep(375); Console.Beep(784, 125); Console.Beep(740, 125); Console.Beep(698, 125); Thread.Sleep(42); Console.Beep(622, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(167); Console.Beep(415, 125); Console.Beep(440, 125); Console.Beep(523, 125); Thread.Sleep(125); Console.Beep(440, 125); Console.Beep(523, 125); Console.Beep(587, 125); Thread.Sleep(250); Console.Beep(784, 125); Console.Beep(740, 125); Console.Beep(698, 125); Thread.Sleep(42); Console.Beep(622, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(167); Console.Beep(698, 125); Thread.Sleep(125); Console.Beep(698, 125); Console.Beep(698, 125); Thread.Sleep(625); Console.Beep(784, 125); Console.Beep(740, 125); Console.Beep(698, 125); Thread.Sleep(42); Console.Beep(622, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(167); Console.Beep(415, 125); Console.Beep(440, 125); Console.Beep(523, 125); Thread.Sleep(125); Console.Beep(440, 125); Console.Beep(523, 125); Console.Beep(587, 125); Thread.Sleep(250); Console.Beep(622, 125); Thread.Sleep(250); Console.Beep(587, 125); Thread.Sleep(250); Console.Beep(523, 125); Thread.Sleep(1125); Console.Beep(784, 125); Console.Beep(740, 125); Console.Beep(698, 125); Thread.Sleep(42); Console.Beep(622, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(167); Console.Beep(415, 125); Console.Beep(440, 125); Console.Beep(523, 125); Thread.Sleep(125); Console.Beep(440, 125); Console.Beep(523, 125); Console.Beep(587, 125); Thread.Sleep(250); Console.Beep(784, 125); Console.Beep(740, 125); Console.Beep(698, 125); Thread.Sleep(42); Console.Beep(622, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(167); Console.Beep(698, 125); Thread.Sleep(125); Console.Beep(698, 125); Console.Beep(698, 125); Thread.Sleep(625); Console.Beep(784, 125); Console.Beep(740, 125); Console.Beep(698, 125); Thread.Sleep(42); Console.Beep(622, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(167); Console.Beep(415, 125); Console.Beep(440, 125); Console.Beep(523, 125); Thread.Sleep(125); Console.Beep(440, 125); Console.Beep(523, 125); Console.Beep(587, 125); Thread.Sleep(250); Console.Beep(622, 125); Thread.Sleep(250); Console.Beep(587, 125); Thread.Sleep(250); Console.Beep(523, 125);
+        }
+        static void TetrisMusic(Object ct)
+        {
+            CancellationToken token = (CancellationToken)ct;
+            Console.Beep(1320, 500);
+            Console.Beep(990, 250); Console.Beep(1056, 250); Console.Beep(1188, 250); Console.Beep(1320, 125);
+            Console.Beep(1188, 125); Console.Beep(1056, 250); Console.Beep(990, 250); Console.Beep(880, 500); Console.Beep(880, 250); Console.Beep(1056, 250); Console.Beep(1320, 500); Console.Beep(1188, 250);
+            Console.Beep(1056, 250); Console.Beep(990, 750); Console.Beep(1056, 250); Console.Beep(1188, 500); Console.Beep(1320, 500); Console.Beep(1056, 500); Console.Beep(880, 500); Console.Beep(880, 500);
+            Thread.Sleep(250);
+            Console.Beep(1188, 500); Console.Beep(1408, 250); Console.Beep(1760, 500); Console.Beep(1584, 250); Console.Beep(1408, 250); Console.Beep(1320, 750); Console.Beep(1056, 250);
+            Console.Beep(1320, 500); Console.Beep(1188, 250); Console.Beep(1056, 250); Console.Beep(990, 500); Console.Beep(990, 250); Console.Beep(1056, 250);
+            Console.Beep(1188, 500); Console.Beep(1320, 500); Console.Beep(1056, 500); Console.Beep(880, 500); Console.Beep(880, 500);
+        }
+
+
+        static void Affiche(Boolean[] flag, int ligne)
+        {
+
+            String[] s = new string[] { "┌───────┐", "│       │", "│       │", "│       │", "└───────┘" };
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                Console.SetCursorPosition(30, 10 + (ligne * 5) + i);
+                for (int j = 0; j < flag.Length; j++)
+                {
+                    if (flag[j]) { Console.BackgroundColor = ConsoleColor.DarkGreen; } else Console.BackgroundColor = ConsoleColor.Black;
+                    Console.Write(s[i]);
+                }
+
+
+            }
+        }
+
+        static void Binaire(int heure, int minute, int seconde)
+        {
+
+            Boolean[] falgH = new Boolean[6];
+            Boolean[] falgM = new Boolean[6];
+            Boolean[] falgS = new Boolean[6];
+            int modulo = 32;
+            int cpt = 0;
+
+            while (modulo != 0)
+            {
+
+                if (heure % modulo != heure)
+                {
+                    falgH[cpt] = true;
+                    heure = heure - modulo;
+                }
+                else
+                {
+                    falgH[cpt] = false;
+                }
+                if (minute % modulo != minute)
+                {
+                    falgM[cpt] = true;
+                    minute = minute - modulo;
+                }
+                else
+                {
+                    falgM[cpt] = false;
+                }
+                if (seconde % modulo != seconde)
+                {
+                    falgS[cpt] = true;
+                    seconde = seconde - modulo;
+                }
+                else
+                {
+                    falgS[cpt] = false;
+                }
+
+                modulo = modulo / 2;
+                cpt++;
+            }
+            Console.BackgroundColor = ConsoleColor.Black;
+            Affiche(falgH, 0);
+            Affiche(falgM, 1);
+            Affiche(falgS, 2);
+        }
+
+        static void Braille(int heure, int minute, int seconde)
+        {
+
+
+
+        }
+
+        static void Digital(int heure, int minute, int seconde)
+        {
+            String[,] number = new string[10, 6]
+            {
+                 { "   ___   ", "  / _ \\  ", " | | | | ", " | | | | ", " | | | | ", "  \\___/  " } ,
+                { "  __     ", " /_ |   ", "  | |    ", "  | |   ", "  | |   ", "  |_|    " },
+                { "  ___   ", " |__ \\  ", "    ) | ", "   / /  ", "  / /_  ", " |____| " },
+                { "  ____   ", " |___ \\  ", "   __) | ", "  |__ <  ", "  ___) | ", " |____/  " } ,
+                { "  _  _   ", " | || |  ", " | || |_ ", " |__   _|", "    | |  ", "    |_|  " } ,
+                { "  _____  ", " | ____| ", " | |__   ", " |___ \\  ", "  ___) | ", " |____/  " } ,
+                { "    __  ", "   / /  ", "  / /_  ", " | '_ \\ ", " | (_) |", "  \\___/ " } ,
+                { "  ______ ", " |____  |", "     / / ", "    / /  ", "   / /   ", "  /_/    " } ,
+                { "   ___   ", "  / _ \\  ", " | (_) | ", "  > _ <  ", " | (_) | ", "  \\___/  " } ,
+                { "   ___  ", "  / _ \\ ", " | (_) |", "  \\__, |", "    / / ", "   /_/  " }
+
+            };
+
+            /* for (int i = 0; i < number.GetLength(0); i++)
+             {
+                 for (int j = 0; j < number.GetLength(1); j++)
+                 {
+                     Console.Write(number.GetLength(1));
+                     Console.WriteLine(number[i,j]);
+                 }
+                 Console.WriteLine(" ");
+             }*/
+
+            string[] seperateur = new string[] { "  ", "  _ ", " (_)", "    ", "  _ ", " (_)" };
+            AfficheDigital(number, heure, 25, 10);
+            AfficheSeparateur(seperateur, 42, 10);
+            AfficheDigital(number, minute, 47, 10);
+            AfficheSeparateur(seperateur, 65, 10);
+            AfficheDigital(number, seconde, 70, 10);
+
+
+        }
+
+        static void AfficheSeparateur(string[] separateur, int left, int top)
+        {
+
+            for (int i = 0; i < separateur.Length; i++)
+            {
+                Console.SetCursorPosition(left, top + i);
+                Console.WriteLine(separateur[i]);
+            }
+        }
+
+        static void AfficheDigital(String[,] number, int valeur, int left, int top)
+        {
+
+            int i = 0;
+            for (int j = 0; j < number.GetLength(1); j++)
+            {
+                Console.SetCursorPosition(left, top + j);
+                Console.WriteLine(number[(valeur / 10), j]);
+                i++;
+            }
+            for (int j = 0; j < number.GetLength(1); j++)
+            {
+                Console.SetCursorPosition(left + i + 2, top + j);
+                Console.WriteLine(number[(valeur % 10), j]);
+            }
+        }
+
+        static void Heure(List<String> pause)
+        {
+
+            Boolean flag = true;
             Console.CursorVisible = false;
             while (true)
             {
@@ -16,74 +234,42 @@ namespace horloge_binaire
                 int heure = Int32.Parse(DateTime.Now.ToString("HH"));
                 int minute = Int32.Parse(DateTime.Now.ToString("mm"));
                 int seconde = Int32.Parse(DateTime.Now.ToString("ss"));
-                Boolean[] falgH = new Boolean[6];
-                Boolean[] falgM = new Boolean[6];
-                Boolean[] falgS = new Boolean[6];
-                int modulo = 32;
-                int cpt = 0;
-                while (modulo != 0)
+
+                foreach (var item in pause)
                 {
-
-                    if (heure % modulo != heure)
+                    if (date.Equals(item))
                     {
-                        falgH[cpt] = true;
-                        heure = heure - modulo;
+                        CancellationTokenSource cts = new CancellationTokenSource();
+                        ThreadPool.QueueUserWorkItem(new WaitCallback(TetrisMusic), cts.Token);
+                        // // Request cancellation.
+                        cts.Cancel();
+                        // Console.WriteLine("Fin du thread");             
+                        cts.Dispose();
                     }
-                    else
-                    {
-                        falgH[cpt] = false;
-                    }
-                    if (minute % modulo != minute)
-                    {
-                        falgM[cpt] = true;
-                        minute = minute - modulo;
-                    }
-                    else
-                    {
-                        falgM[cpt] = false;
-                    }
-                    if (seconde % modulo != seconde)
-                    {
-                        falgS[cpt] = true;
-                        seconde = seconde - modulo;
-                    }
-                    else
-                    {
-                        falgS[cpt] = false;
-                    }
-
-                    modulo = modulo / 2;
-                    cpt++;
                 }
-                Console.BackgroundColor = ConsoleColor.Black;
-                Console.SetCursorPosition(50, 5);
-                Console.WriteLine(date);
-                Affiche(falgH,0);
-                Affiche(falgM,1);
-                Affiche(falgS,2);
-                Console.SetCursorPosition(0, 0);
-                Thread.Sleep(100);
-            }
-        }
 
 
-
-
-
-        static void Affiche(Boolean[] flag,int ligne)
-        {
-
-            String[] s = new string[] { "┌───────┐", "│       │", "│       │", "│       │", "└───────┘" };
-
-            for (int i = 0; i < s.Length; i++)
-            {
-                Console.SetCursorPosition(30, 10+(ligne * 5) + i);
-                for (int j = 0; j < flag.Length; j++)
+                if (flag)
                 {
-                    if (flag[j]) { Console.BackgroundColor = ConsoleColor.DarkGreen; } else Console.BackgroundColor = ConsoleColor.Black;
-                    Console.Write(s[i]);
+                    Digital(heure, minute, seconde);
                 }
-                
+                else
+                {
+                    Binaire(heure, minute, seconde); Console.SetCursorPosition(50, 5);
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.WriteLine(date);
+                }
+
+
+
+                //
+
+
+
+                Thread.Sleep(1000);
+
+
+
 
             }
         }
